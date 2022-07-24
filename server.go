a2940d12a77d2c9e737076599f527547cec54434
pchchv <ipchchv@gmail.com>
2022-07-24 17:07:24 +0300

    Implement a ping method

--- server.go
+++ server.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+func ping(c echo.Context) error {
+	msg := "Passholder. Version 0.0.0"
+	return c.String(http.StatusOK, msg)
+}
+
+func routes(e *echo.Echo) {
+	e.GET("/ping", ping)
+}
+
 func server() {
 	e := echo.New()
+	routes(e)
 	log.Fatal(e.Start(envURL))
 }
